29a27e080c4a65c00248b262b14dfece8aac4657
Mike Hanney <mrh5937@gmail.com>
2017-12-13 13:20:25 -0800

    added text messages and cadence targets

--- zwoparse.py
+++ zwoparse.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Parse a Zwift Workout XML file and output a plain text file description or csv.
+Parse a Zwift Workout XML file and output a plain text file, csv or json describing the workout.
 
 Usage:
 ======
@@ -19,6 +19,8 @@ optional arguments:
   -k KG, --kg KG        The rider's weight in kilograms to the nearest
                         integer, e.g. 71
   -v, --verbose         Verbose - also output to stdout
+  -m, --min-duration    Minimum duration allowed for a block of work (in seconds). 0 means no minimum. For values above 0 an attempt will be made to combine 
+                        workout sections together until the minimum duration specified is met.
   -o OUTFILE, --outfile OUTFILE
                         The name of the output file, defauts to workout.txt if
                         none given
@@ -37,27 +39,31 @@ class Power:
         self.min_intensity = min_intensity
         self.max_intensity = max_intensity
 
-    def toJSON(self):
-        """ dumps self as json """
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
+
+class TextEvent:
+    """ A simple class to represent a text event with timeoffset and message.
+    timeoffset is seconds from the beginning of the workout. 
+    timeoffset_relative is seconds form the beginning of the segment in which the 
+    textmessage appears (useful for auto-hotkey timings)"""
+
+    def __init__(self, timeoffset, timeoffset_relative, message):
+        self.timeoffset = timeoffset
+        self.timeoffset_relative = timeoffset_relative
+        self.message = message
 
 
 class Segment:
     """ A simple class to represent a segment of a workout """
 
-    def __init__(self, start_time, end_time, segment_type, power, cadence=None, working=False):
+    def __init__(self, start_time, end_time, segment_type, power, cadence, working=False):
         self.start_time = start_time
         self.end_time = end_time
         self.segment_type = segment_type
         self.working = working
-        self.power = power
         self.duration_ms = (end_time - start_time) * 1000
-
-        if cadence is None:
-            self.cadence = 0
-        else:
-            self.cadence = cadence
+        self.textevents = []
+        self.power = power
+        self.cadence = cadence
 
     def duration(self):
         """ returns the duration of the segment of the workout """
@@ -91,11 +97,19 @@ class Segment:
         elif self.segment_type == "steadystate":
             return "Steady state"
 
+        elif self.segment_type == "combined":
+            return "Combined"
+
         elif self.segment_type == "intervalst":
             if self.working:
                 return "Work Interval"
             else:
                 return "Rest Interval"
+
+    def add_text_event(self, timeoffset, message):
+        timeoffset_relative = timeoffset - self.start_time
+        self.textevents.append(
+            TextEvent(timeoffset, timeoffset_relative, message))
 
     def toJSON(self):
         """ dumps self as json """
@@ -166,7 +180,23 @@ def parse_interval_power(node, state):
     return Power(min_intensity, max_intensity)
 
 
-def parse(xmlstring):
+def parse_cadence(node, suffix=""):
+    if node.tag == "FreeRide":
+        return None
+
+    return node.get("Cadence" + suffix)
+
+
+def parse_textevents(node, segment):
+    for textevent in node.iter("textevent"):
+        timeoffset = int(textevent.get("timeoffset"))
+        message = textevent.get("message")
+        segment.add_text_event(timeoffset, message)
+
+    return segment
+
+
+def parse(xmlstring, minduration=0):
     segments = []
     tree = ET.ElementTree(ET.fromstring(xmlstring))
     root = tree.getroot()
@@ -187,11 +217,14 @@ def parse(xmlstring):
                 or lower_tag == "freeride" \
                 or lower_tag == "steadystate":
             duration = node.get("Duration")
-            cadence = node.get("Cadence")
+            cadence = parse_cadence(node)
             end_time = time + round_to_nearest_second(duration)
             power = parse_power(node)
-            segments.append(Segment(time, end_time, lower_tag, power, cadence))
-            time = end_time + 1
+            segment = Segment(time, end_time, lower_tag,
+                              power, cadence)
+            time = end_time
+            parse_textevents(node, segment)
+            segments.append(segment)
 
         elif lower_tag == "intervalst":
             repeat = int(node.get("Repeat"))
@@ -199,19 +232,48 @@ def parse(xmlstring):
             off_duration = float(node.get("OffDuration"))
             on_power = parse_interval_power(node, "On")
             off_power = parse_interval_power(node, "Off")
-            cadence = node.get("Cadence")
-            cadence_resting = node.get("CadenceResting")
+            cadence_work = parse_cadence(node)
+            cadence_rest = parse_cadence(node, "Resting")
 
             for i in range(0, repeat):
                 end_time = time + round_to_nearest_second(on_duration)
-                segments.append(
-                    Segment(time, end_time, lower_tag, on_power, cadence, True))
-                time = end_time + 1
+                segment = Segment(time, end_time, lower_tag,
+                                  on_power, cadence_work, True)
+                time = end_time
+                parse_textevents(node, segment)
+                segments.append(segment)
 
                 end_time = time + round_to_nearest_second(off_duration)
-                segments.append(
-                    Segment(time, end_time, lower_tag, off_power, cadence_resting, False))
-                time = end_time + 1
+                segment = Segment(time, end_time, lower_tag,
+                                  off_power, cadence_rest, False)
+                time = end_time
+                parse_textevents(node, segment)
+                segments.append(segment)
+
+    if minduration:
+        # if minduration greater than zero, combine sections before returning segments
+        for i in range(len(segments) - 1, 0, -1):
+            seg_current = segments[i]
+            seg_previous = segments[i - 1]
+            if seg_current.duration_ms < minduration * 1000:
+                seg_previous.end_time = seg_previous.end_time + \
+                    (seg_current.duration_ms / 1000)
+
+                seg_previous.duration_ms = (
+                    seg_previous.end_time - seg_previous.start_time) * 1000
+
+                # clear any text events, because they won't make sense
+                seg_previous.textevents = []
+
+                if seg_current.power > seg_previous.power:
+                    seg_previous.power = seg_current.power
+
+                if seg_current.cadence > seg_previous.cadence:
+                    seg_previous.cadence = seg_current.cadence
+
+                seg_previous.segment_type = 'combined'
+
+                segments.pop(i)
 
     return {'name': name, 'description': description, 'segments': segments}
 
@@ -224,6 +286,7 @@ def main():
     filetype = "txt"
     filename = "workout"
     verbose = True
+    minduration = 0
 
     parser = argparse.ArgumentParser(description=(
         """Converts a Zwift Workout File to csv or plain text file."""))
@@ -245,6 +308,12 @@ def main():
         "--verbose",
         type=bool,
         help="Also output to stdout")
+
+    parser.add_argument(
+        "-m",
+        "--minduration",
+        type=int,
+        help="Minimum duration allowed for a block of work  (in seconds). 0 means no minimum. For values above 0 an attempt will be made to combine workout sections together until the minimum duration specified is met. (Useful for fitting music to workout sections)")
 
     parser.add_argument(
         "-t",
@@ -276,6 +345,9 @@ def main():
     if args.type != None:
         filetype = args.type
 
+    if args.minduration != None:
+        minduration = args.minduration
+
     if args.outfile != None:
         outfile_with_extension = args.outfile
     else:
@@ -285,15 +357,15 @@ def main():
 
     with args.file as input_file:
         xmlstring = input_file.read()
-        workout = parse(xmlstring)
+        workout = parse(xmlstring, minduration)
 
         if filetype == "csv":
-            lines.append('Type, StartTime, EndTime, Duration, Duration Formatted, Min Power (% FTP), Min Power (W), Min Power (W/Kg), Max Power  (% FTP), Min Power (W), Min Power (W/Kg), Tempo, Work\n')
+            lines.append('Type, StartTime, EndTime, Duration, Duration Formatted, Min Power (% FTP), Min Power (W), Min Power (W/Kg), Max Power  (% FTP), Min Power (W), Min Power (W/Kg), Cadence, Work\n')
 
             for segment in workout['segments']:
-                lines.append('%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n' % (segment.segment_type, segment.human_type(), segment.start_time,
-                                                                                           segment.end_time, segment.duration(), segment.human_duration(),
-                                                                                           round_to_percentage(
+                lines.append('%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n' % (segment.segment_type, segment.human_type(), segment.start_time,
+                                                                              segment.end_time, segment.duration(), segment.human_duration(),
+                                                                              round_to_percentage(
                     segment.power.min_intensity),
                     convert_to_abs_power(
                     segment.power.min_intensity, ftp),
@@ -307,6 +379,8 @@ def main():
                     segment.power.max_intensity, ftp, kilos),
                     segment.cadence, segment.working))
 
+                # textevents are not written to csv because csv is not hierarchic
+
         elif filetype == "json":
             segments_json = ','.join([x.toJSON() for x in workout['segments']])
             lines.append('{"name": %s, "description" : %s, "segments":[%s]}' % (
@@ -318,23 +392,32 @@ def main():
             lines.append("%s\n\n" % workout['description'])
 
             for segment in workout['segments']:
+
+                cadence_str = ""
+                if segment.cadence is not None:
+                    cadence_str = cadence_str + "%s RPM " % segment.cadence
+
                 if segment.power.min_intensity:
-                    intensity = "from %s%% FTP to %s%% FTP (%s W to %s W, %s W/Kg to %s W/Kg)" % (
+                    intensity = "from %s%% FTP to %s%% FTP %s(%s W to %s W, %s W/Kg to %s W/Kg)" % (
                         round_to_percentage(segment.power.min_intensity),
                         round_to_percentage(segment.power.max_intensity),
+                        cadence_str,
                         convert_to_abs_power(segment.power.min_intensity, ftp),
                         convert_to_abs_power(segment.power.max_intensity, ftp),
                         convert_to_watts_per_kilo(
                             segment.power.min_intensity, ftp, kilos),
                         convert_to_watts_per_kilo(segment.power.max_intensity, ftp, kilos))
                 else:
-                    intensity = "%s%% FTP (%s W, %s W/Kg)" % (round_to_percentage(segment.power.max_intensity),
-                                                              convert_to_abs_power(
-                                                                  segment.power.max_intensity, ftp),
-                                                              convert_to_watts_per_kilo(segment.power.max_intensity, ftp, kilos))
-
+                    intensity = "%s%% FTP %s(%s W, %s W/Kg)" % (round_to_percentage(segment.power.max_intensity),
+                                                                cadence_str,
+                                                                convert_to_abs_power(
+                        segment.power.max_intensity, ftp),
+                        convert_to_watts_per_kilo(segment.power.max_intensity, ftp, kilos))
                 lines.append("%s %s for %s\n" % (
                     segment.human_type(), intensity, segment.human_duration()))
+
+                for textevent in segment.textevents:
+                    lines.append("\t%s\n" % (textevent.message))
 
         text_file = open(outfile_with_extension, "w")
         for line in lines:
